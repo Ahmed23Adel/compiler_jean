c79f3e1d982c8495dbb278a0e838e5cc54255dbb
EzzatEsam <s-ezzat.eisawy@zewailcity.edu.eg>
2023-05-05 09:49:05 +0300

    .

---
+++ parser.go
@@ -0,0 +1,43 @@
+package main
+
+import(
+	"errors"
+	"fmt"
+)
+
+type parserFunction func(start int ,tokenArray  []Token ) (end int,node *Node ,err error)
+
+func parseSequential(start int ,parsers []parserFunction ,tokenArray  []Token ) (end int,currentNode *Node ,err error) {
+	startPoint := start
+
+
+	currentNode = &Node{startPoint,startPoint,"",[]*Node{}}
+	for _,parser := range parsers {
+		end ,child ,err := parser(startPoint,tokenArray)
+		if err != nil {
+			return -1 , nil ,errors.New("failed to parse")
+		}
+		startPoint = end
+		currentNode.adjacent = append(currentNode.adjacent,child)
+		currentNode.end = end
+	}
+	return startPoint , currentNode ,nil
+}
+
+
+func parseDocument(tokenArray []Token)  {  
+	end ,CFG ,err := codeParser(0,tokenArray)
+	if err != nil   {
+		fmt.Println("Failed to parse")
+	} else if end != len(tokenArray) {
+		PrintGraph(CFG, tokenArray)
+		println("parser finished before the end of the document")
+		println("parser ended at ",end , " and document ended at ",len(tokenArray))
+	}else {
+		printTree(CFG , tokenArray)
+		PrintGraph(CFG, tokenArray)
+		println("parser succeeded")
+	}
+
+	
+}
